d028fe59fd7ffba145c61c1e524d5e4deaf9d2d4
Andrey Churenkov <andrey.churenkov@gmail.com>
2022-07-27 16:29:58 +0300

    update ItemsController, ItemRepository

--- src/main/java/com/example/multicardtesttask/controllers/item/ItemsController.java
+++ src/main/java/com/example/multicardtesttask/controllers/item/ItemsController.java
@@ -1,12 +1,11 @@
 package com.example.multicardtesttask.controllers.item;
 
+import com.example.multicardtesttask.models.Item;
 import com.example.multicardtesttask.repositories.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/items")
@@ -20,9 +19,30 @@ public class ItemsController {
     }
 
     @GetMapping("/{id}")
-    public String getItems(@PathVariable int id, Model model) {
+    public String getItems(Model model, @PathVariable("id") int id) {
         model.addAttribute("items", repository.findAllBySale_Id(id));
 
         return "items";
+    }
+
+    @PostMapping("/{id}")
+    public void createOrUpdateItems(@ModelAttribute("item") Item item) {
+        repository.save(item);
+    }
+
+    // Самый продаваемый товар за месяц
+    @GetMapping("/top_by_month")
+    public String getTopItemByMonth(Model model) {
+        model.addAttribute("item", repository.findTopItemByMonth());
+
+        return "report/top_item_by_month";
+    }
+
+    // Самый продаваемый товар у людей в возрасте 18 лет
+    @GetMapping("/top_by_buyer18")
+    public String getTopItemByBuyer18(Model model) {
+        model.addAttribute("item", repository.findTopItemByBuyer18());
+
+        return "report/top_item_by_buyer18";
     }
 }
--- src/main/java/com/example/multicardtesttask/repositories/ItemRepository.java
+++ src/main/java/com/example/multicardtesttask/repositories/ItemRepository.java
@@ -12,4 +12,27 @@ import java.util.List;
 public interface ItemRepository extends JpaRepository<Item, Integer> {
 
     List<Item> findAllBySale_Id(int id);
+
+    // Самый продаваемый товар за месяц
+    @Query(value = "SELECT items.name\n" +
+            "FROM items\n" +
+            "WHERE items.sale_id IN (SELECT sales.id\n" +
+            "FROM sales\n" +
+            "WHERE sales.purchase_date BETWEEN(current_date - interval'1 month') " +
+            "AND (current_date))\n" +
+            "GROUP BY items.name\n" +
+            "ORDER BY COUNT(items.name) DESC\n" +
+            "LIMIT 1", nativeQuery = true)
+    String findTopItemByMonth();
+
+    // Самый продаваемый товар у людей в возрасте 18 лет
+    @Query(value = "SELECT name\n" +
+            "FROM items\n" +
+            "WHERE sale_id IN (SELECT sales.id FROM sales\n" +
+            "                 WHERE sales.age = '18')\n" +
+            "GROUP BY name\n" +
+            "ORDER BY count(name) DESC\n" +
+            "LIMIT 1", nativeQuery = true)
+    String findTopItemByBuyer18();
+
 }
